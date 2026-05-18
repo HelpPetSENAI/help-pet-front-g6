418a82cdab0654f1c54ec349e6703a4e7a84b23a
th-cs <202718515+th-cs@users.noreply.github.com>
2026-05-18 04:44:27 -0300

    refactor: add props to ChatList component

--- src/pages/page-g6/components/ChatListElement.jsx
+++ src/pages/page-g6/components/ChatListElement.jsx
@@ -2,16 +2,16 @@ import { StyledChatListElement } from "../styles/ChatListElementStyles.js";
 import Avatar from '../../../assets/icons/G6GreenBall';
 import Badge from '../../../assets/icons/NewMessageBadge';
 
-export default function ChatListElement({ onlyClick }) {
+export default function ChatListElement({ onlyClick, userName, lastMessage, lastMessageAt }) {
     return (
         <StyledChatListElement onClick={onlyClick}>
                 <Avatar className="chat-avatar" aria-label="Avatar" />
                 <div className="chat-data">
-                    <span className="chat-username">Pessoa</span>
-                    <span className="chat-last-message">A mensagem não aparece toda pois aparece "..."</span>
+                    <span className="chat-username">{userName}</span>
+                    <span className="chat-last-message">{lastMessage}</span>
                 </div>
                 <div className="chat-right">
-                    <span className="last-message-time">12:00</span>
+                    <span className="last-message-time">{lastMessageAt}</span>
                     <Badge className="chat-notification-badge" aria-label="Mensagem não lida" />
                 </div>
         </StyledChatListElement>
