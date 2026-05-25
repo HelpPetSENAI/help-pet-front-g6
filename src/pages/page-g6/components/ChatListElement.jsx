4a40f68a802a54b11f52e6a8efb9385a7fd0b7c6
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-25 10:35:28 -0300

    fix: new message bag

--- src/pages/page-g6/components/ChatListElement.jsx
+++ src/pages/page-g6/components/ChatListElement.jsx
@@ -2,7 +2,7 @@ import { StyledChatListElement } from "../styles/ChatListElementStyles.js";
 import Avatar from '../../../assets/icons/G6GreenBall';
 import Badge from '../../../assets/icons/NewMessageBadge';
 
-export default function ChatListElement({ onlyClick }) {
+export default function ChatListElement({ onlyClick, hasUnread }) {
     return (
         <StyledChatListElement onClick={onlyClick}>
                 <Avatar className="chat-avatar" aria-label="Avatar" />
@@ -12,7 +12,7 @@ export default function ChatListElement({ onlyClick }) {
                 </div>
                 <div className="chat-right">
                     <span className="last-message-time">12:00</span>
-                    <Badge className="chat-notification-badge" aria-label="Mensagem não lida" />
+                    {hasUnread && <Badge className="chat-notification-badge" aria-label="Mensagem não lida" />}
                 </div>
         </StyledChatListElement>
     )
